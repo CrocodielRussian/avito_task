63b538f5ad20f6deac82187330a3d0f1b7dae5ff
CrocodielRussian <mk-spbkiselev@yandex.ru>
2025-11-16 22:22:29 +0300

    fix: rewrite types

--- types/models.go
+++ types/models.go
@@ -1,20 +1,38 @@
 package types
 
-type User struct {
+type Team struct {
 	ID       string
-	IsActive bool
+	TeamName string       `json:"team_name"`
+	Members  []TeamMember `json:"members"`
 }
 
-type Team struct {
-	ID    string
-	Name  string
-	Users []User
+type TeamMember struct {
+	ID       string
+	UserID   string `json:"user_id"`
+	Username string `json:"username"`
+	IsActive bool   `json:"is_active"`
+}
+
+type User struct {
+	UserID   string  `json:"user_id"`
+	Username string  `json:"username"`
+	TeamName *string `json:"team_name"`
+	IsActive bool    `json:"is_active"`
 }
 
 type PullRequest struct {
-	ID         string
-	AuthorID   string
-	ReviewerID string
-	TeamID     string
-	Status     string
+	ID                string
+	PullRequestID     string   `json:"pull_request_id"`
+	PullRequestName   string   `json:"pull_request_name"`
+	AuthorID          string   `json:"author_id"`
+	Status            string   `json:"status"`
+	AssignedReviewers []string `json:"assigned_reviewers"`
+}
+
+type PullRequestShort struct {
+	ID              string
+	PullRequestID   string `json:"pull_request_id"`
+	PullRequestName string `json:"pull_request_name"`
+	AuthorID        string `json:"author_id"`
+	Status          string `json:"status"`
 }
