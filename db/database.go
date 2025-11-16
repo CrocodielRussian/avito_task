36e88aec71e913b20d36033d6b9a987850a61dd9
CrocodielRussian <mk-spbkiselev@yandex.ru>
2025-11-16 22:53:35 +0300

    fix: change port of database

--- db/database.go
+++ db/database.go
@@ -7,6 +7,6 @@ import (
 )
 
 func NewPostgresPool(ctx context.Context) (*pgxpool.Pool, error) {
-	dbURL := "postgres://postgres:postgres@localhost:5432/pr_service?sslmode=disable"
+	dbURL := "postgres://postgres:postgres@localhost:5435/pr_database?sslmode=disable"
 	return pgxpool.New(ctx, dbURL)
 }
